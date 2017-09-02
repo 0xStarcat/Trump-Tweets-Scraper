9f52ec3aed7425f16453d2dd66a652c15b4755a9
Jeff <jeffahking@gmail.com>
2017-09-02 11:05:10 -0400

    update scraper

--- index.js
+++ index.js
@@ -2,43 +2,37 @@
 // Here's a quick tutorial that got me started https://scotch.io/tutorials/scraping-the-web-with-node-js
 // And the documentation for our scraper, Cheerio, that can help if we get stuck! https://cheerio.js.org/
 
-var express = require('express');
 var fs = require('fs');
 var request = require('request-promise');
 var cheerio = require('cheerio');
-var app     = express();
 var assign = require('./lib/assigners.js')
 var fs = require('fs');
 
 var trumpTweets = []
 
-app.get('/', function(req, res) {
-  res.send('WELCOME TO THE WEB! Go to "/scrape" to start the scrape!')
-})
 
-app.get('/scrape', function(req, res) {
-   url = 'https://www.nytimes.com/interactive/2016/01/28/upshot/donald-trump-twitter-insults.html';
+function scrapeTime() {
+  url = 'https://www.nytimes.com/interactive/2016/01/28/upshot/donald-trump-twitter-insults.html';
 
    // We are going to use a promise so we can completely construct our object before we send it to D3 for viz processing
    // Due to the multiple asynchronous requests we are making, if we don't use promises,
    // the code would send the object before it's filled with data
 
-   // An Express/Promise lesson https://coderwall.com/p/9cifuw/scraping-web-pages-using-node-js-using-request-promise
+   // An request/Promise lesson https://coderwall.com/p/9cifuw/scraping-web-pages-using-node-js-using-request-promise
 
    requestPromise(url).then(function(data) {
       var fileName = '../TRPtweets/server/db/trumptweets.json'
+      
      // Write the data to a file when asynchronous request is resolved
       fs.writeFile(fileName, JSON.stringify(trumpTweets), function(err) {
         if(err) { return console.log(err); }
         console.log("The file was saved to " + fileName);
       });
 
-     res.send('scrape complete - check the console')
+     console.log('Scrape complete!!')
 
    }, function(error) {
-        res.send(error)
-        console.error("%s; %s", err.message, url);
-        console.log("%j", err.res.statusCode);
+        console.log('error!!!', error)
    })
 
   function requestPromise(url) {
@@ -46,6 +40,7 @@ app.get('/scrape', function(req, res) {
        //NYtimes leads to infinite redirects because there are no cookies enabled apparently.
        //solution: pass jar:true into Request
        //https://github.com/request/request/issues/1809
+
        request({url, jar: true}, function(error, response, html) {
           if(!error) {
             var $ = cheerio.load(html);
@@ -57,11 +52,11 @@ app.get('/scrape', function(req, res) {
         })
      })
   }
-})
+}
 
-//Scraping functions
 function scrapeEntities($) {
   var entities = []
+
   $('.g-entity-item').each(function(i, elem) {
     var entity = {
       name: "",
@@ -70,6 +65,7 @@ function scrapeEntities($) {
       gender: "",
       tweets: []
     }
+
     $(elem).find('.g-entity-name').each(function(i, elem) {
       entity.name = $(this).text()
     })
@@ -87,8 +83,10 @@ function scrapeEntities($) {
       entity.count = tweetSet.length
 
     });
+
     entities.push(entity)
   });
+
   assignEntities(entities)
 }
 
@@ -101,10 +99,4 @@ function assignEntities(entities) {
   console.log('Missing assignments: ' + assign.missing)
 }
 
-
-
-app.listen('8000')
-
-console.log('Magic happens on port 8000');
-
-exports = module.exports = app;
+scrapeTime()
